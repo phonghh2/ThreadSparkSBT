d5c196c978c23f2989b3adb9751b348e9e92d6e4
phonghh2 <phonghh1989@gmail.com>
2015-08-07 11:09:06 +0700

    aaaaaaaaaaaa

--- src/main/scala/TheadSpark/ThreadSpark.scala
+++ src/main/scala/TheadSpark/ThreadSpark.scala
@@ -8,10 +8,11 @@ import java.net.URI
 
 import com.mongodb.{Mongo, ServerAddress}
 import com.redis._
+import net.liftweb.common.Full
 import net.liftweb.mongodb._
-import net.liftweb.mongodb.record.field.StringPk
-import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
-import net.liftweb.record.field.{DoubleField, StringField}
+import net.liftweb.mongodb.record.field.{BsonRecordListField, StringRefField, StringPk}
+import net.liftweb.mongodb.record.{BsonMetaRecord, BsonRecord, MongoMetaRecord, MongoRecord}
+import net.liftweb.record.field.{IntField, DoubleField, StringField}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -51,10 +52,27 @@ object ThreadSpark {
         for(x<-DBListModel){
           RangeScoring(x.id.toString(), hiveContext, x.name.toString())
         }
-        TopBotOption("d848e3f9-9ae6-4c46-ba46-62adb892e94d", hiveContext)
-        TopBotOption("878578e5-c9f4-430e-a129-446eaa69b374", hiveContext)
-        TopBotOption("1a021ec7-83af-46ff-af31-70ba8600ff77", hiveContext)
-        TopBotOption("9daa9841-c94a-498d-9a32-28cd8f91ec80", hiveContext)
+
+        val DBListFT = Factor.findAll
+
+        var listDBCuoi: List[Factor] = List()
+
+        for (factor <- DBListFT) {
+          if (factor.FactorOption.value.size != 0)
+            listDBCuoi = listDBCuoi ::: List(factor)
+        }
+
+        for(x<-listDBCuoi.distinct){
+          for(y<-x.FactorOption.value.distinct){
+            if(y.FactorOptionId.toString().equals("") == false)
+              TopBotOption(y.FactorOptionId.toString(), hiveContext)
+          }
+        }
+//
+//        TopBotOption("d848e3f9-9ae6-4c46-ba46-62adb892e94d", hiveContext)
+//        TopBotOption("878578e5-c9f4-430e-a129-446eaa69b374", hiveContext)
+//        TopBotOption("1a021ec7-83af-46ff-af31-70ba8600ff77", hiveContext)
+//        TopBotOption("9daa9841-c94a-498d-9a32-28cd8f91ec80", hiveContext)
       }
     }
     thread.start
@@ -106,5 +124,62 @@ object ThreadSpark {
   object ModelInfo extends ModelInfo with MongoMetaRecord[ModelInfo] {
     override def collectionName = "modelinfo"
   }
+
+  class Factor private () extends MongoRecord[Factor] with StringPk[Factor] {
+
+    override def meta = Factor
+
+    object ModelId extends StringRefField(this, ModelInfo, 512){
+      override def options = ModelInfo.findAll.map(rd => (Full(rd.id.is), rd.name.is) )
+    }
+    object Parentid extends StringRefField(this, Factor, 512){
+      override def options = Factor.findAll.map(rd => (Full(rd.id.is), rd.FactorName.is) )
+    }
+    //  object Parentid extends MongoRefField(this)
+    object ParentName extends StringField(this, 512)
+    object FactorName extends StringField(this, 512)
+    object Description extends StringField(this, 512)
+    object Weight extends DoubleField(this)
+    object Ordinal extends IntField(this)
+    object Status extends StringField(this, 512)
+    object Note extends StringField(this, 512)
+    object PercentTotal extends DoubleField(this)
+    object PathFactor extends BsonRecordListField(this, FactorPath)
+
+    object FactorOption extends BsonRecordListField(this, FactorOptionIN)
+
+    // An embedded document:
+    //  object factor extends BsonRecordListField(this, factorIN)
+
+  }
+
+  object Factor extends Factor with MongoMetaRecord[Factor] {
+    override def collectionName = "factor"
+  }
+
+  class FactorPath private () extends BsonRecord[FactorPath] {
+    def meta = FactorPath
+    object FactorPathId extends StringRefField(this, Factor, 512){
+      override def options = Factor.findAll.map(rd => (Full(rd.id.is), rd.FactorName.is) )
+    }
+    //  object FactorPathId extends StringField(this, 512)
+    object Weight extends DoubleField(this)
+
+  }
+
+  object FactorPath extends FactorPath with BsonMetaRecord[FactorPath]
+
+  class FactorOptionIN private () extends BsonRecord[FactorOptionIN] {
+    def meta = FactorOptionIN
+    object FactorOptionId extends StringField(this, 50)
+    object FactorOptionName extends StringField(this, 512)
+    object Description extends StringField(this, 512)
+    object Score extends DoubleField(this)
+    object Fatal extends StringField(this, 512)
+    object Status extends StringField(this, 512)
+
+  }
+
+  object FactorOptionIN extends FactorOptionIN with BsonMetaRecord[FactorOptionIN]
 }
 
